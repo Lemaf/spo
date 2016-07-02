2ccf7bc0bdd228c3457db07d89ab5615691deb43
Vitor Lobo Ramos <lobocode@gmail.com>
2016-07-01 23:26:19 -0300

    find and move older folder

--- sys/olderDir.py
+++ sys/olderDir.py
@@ -3,21 +3,19 @@
 import os
 import time
 import shutil
+import glob
 
 # 86400 = 24 hours = 1 day
-
 oneday = 86400
 numdays = oneday*15
-
 now = time.time()
-directory=os.path.join(".")
 
-for r,d,f in os.walk(directory):
-    for dir in d:
-         timestamp = os.path.getmtime(os.path.join(r,dir))
-         if now-numdays > timestamp:
-			 try:
-				 oldFolders = os.path.join(r,dir)
-				 shutil.move(oldFolders,"backup")
-			 except shutil.Error:
-				 pass
+# Directory name
+dirName=glob.glob("teste*")
+
+for x in dirName:
+	timestamp = os.path.getmtime(os.path.join(x))
+	if now-numdays > timestamp:
+		oldFolders = os.path.join(x)
+		shutil.move(oldFolders,"backup")
+
