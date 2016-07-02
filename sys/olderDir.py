2b9fd603a2d3b9844b370b32dceaeb384f9d90e7
Vitor Lobo Ramos <lobocode@gmail.com>
2016-07-01 22:25:08 -0300

    Fix try error

--- sys/olderDir.py
+++ sys/olderDir.py
@@ -4,7 +4,11 @@ import os
 import time
 import shutil
 
-numdays = 86400*7
+# 86400 = 24 hours = 1 day
+
+oneday = 86400
+numdays = oneday*15
+
 now = time.time()
 directory=os.path.join(".")
 
@@ -12,11 +16,8 @@ for r,d,f in os.walk(directory):
     for dir in d:
          timestamp = os.path.getmtime(os.path.join(r,dir))
          if now-numdays > timestamp:
-             try:
-                  print "removing ",os.path.join(r,dir)
-                  # shutil.rmtree(os.path.join(r,dir))  #uncomment to use
-             except Exception,e:
-                  print e
-                  pass
-             else: 
-                  print "some message for success"
+			 try:
+				 oldFolders = os.path.join(r,dir)
+				 shutil.move(oldFolders,"backup")
+			 except shutil.Error:
+				 pass
